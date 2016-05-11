8797076ced1fe1c94df906417f3958b3cb5b67c2
Jamilah <jlwatt7@gmail.com>
2016-05-10 22:12:15 -0400

    Initial push

---
+++ src/PracticeMakesPerfect.java
@@ -0,0 +1,59 @@
+
+public class PracticeMakesPerfect {
+
+	public static void main(String[] args) {
+	
+		int x = 2;
+		int y = 8;	
+		int multiply = x*y;
+		
+		System.out.println( (x) + (y) + " = " + multiply);
+		
+			
+	}
+}
+
+/*adding to a number (getting the odd numbers
+ int sum =0
+ for (int i=1; i<=n; i+=2)
+ if (i%2!=0) <-- you dont have to use this but it essentially says if the variable isn't divisible by 2 it's odd
+ {sum = sum +1}
+ sysout(sum);
+ */
+
+/*using char have 'A' or 'B' print "very good"...'C' or 'D' print "good"...anything else print "fail"
+ 	if (letterGrade == 'A' || letterGrade == 'B') 
+	 	{
+	 	System.out.println ("Very Good");
+	 	}
+ 	else if (letterGrade == 'C' || letterGrade == 'D') 
+		 {
+		 System.out.println ("Good");
+		 }
+ else 
+		 {
+		 System.out.println("Fail");
+		 }
+ 
+ */
+
+/*String text = "Zetor";
+		int wholeNumber = 90000;
+		double decimalNumber = 0.1;
+		System.out.println("Chicken:\n" + wholeNumber);
+		System.out.println("Bacon (kg):\n" + decimalNumber);
+		System.out.println("A tractor:\n" + text);
+		System.out.println();
+		System.out.println("In a nutshell:\n" + wholeNumber+ "\n" + decimalNumber + "\n" + text);*/
+
+/* int days = 365;
+		int hours = 24;
+		int minutes = 60;
+		int seconds = 60;
+		int sum = days*hours*minutes*seconds;
+		System.out.println("There are " + sum + " seconds in year");*/
+
+/*int one = 5;
+int two = 4;
+int sum = one + two;
+System.out.println("5 + 4 = " + sum);*/
